cd3a795a61d7d1a8830de7d20b65cdbece2b65e3
Martin Page <wookoouk@gmail.com>
2016-10-26 11:00:57 +0100

    test

--- models/knowledge/document.js
+++ models/knowledge/document.js
@@ -1,12 +1,20 @@
 const thinky = require('../../lib/thinky');
 const type = thinky.type;
+const r = type.r;
 
 
 const Document = thinky.createModel('Document', {
     id: type.string(),
     title: type.string().required(),
     body: type.string().required().default('# New Document'),
-    username: type.string().required().default('Unknown')
+    username: type.string().required().default('Unknown'),
+    createdAt: type.date().default(r.now()),
+    updatedAt: type.date().default(null)
+
+});
+
+Document.on("saved", function () {
+    this.updatedAt = new Date();
 });
 
 module.exports = Document;
