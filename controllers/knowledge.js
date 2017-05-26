a723debd130c07801ad565977df45ade2129e80b
Martin Page <wookoouk@gmail.com>
2017-05-26 14:05:12 +0100

    test

--- controllers/knowledge.js
+++ controllers/knowledge.js
@@ -7,7 +7,7 @@ const Document = require('../models/knowledge/document');
 const Upload = require('../models/knowledge/upload');
 
 Knowledge.index = (req, res) => {
-    Document.filter(r.row.hasFields('versionOf')).then((docs) => {
+    Document.filter(r.row.hasFields('versionOf').not()).then((docs) => {
 
         docs = docs.sort(function (a, b) {
             if (a.title < b.title) return -1;
